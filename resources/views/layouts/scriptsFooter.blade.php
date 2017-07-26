c33e1595d525a2f8a310c26dbe493376e39fff11
Nicholas Kute <antiv_boy@yahoo.com>
2017-07-27 00:16:33 +0300

    scripts path

--- resources/views/layouts/scriptsFooter.blade.php
+++ resources/views/layouts/scriptsFooter.blade.php
@@ -1,41 +1,41 @@
 
 
-<script src="/myjs/jquery.min.js"></script>
+<script src="{{ asset('myjs/jquery.min.js') }}"></script>
 
-<script src="/myjs/bootstrap.min.js"></script>
+<script src="{{ asset('myjs/bootstrap.min.js') }}"></script>
 
-<script src="/myjs/jasny-bootstrap.min.js"></script>
+<script src="{{ asset('myjs/jasny-bootstrap.min.js') }}"></script>
 
-<script src="/myjs/jquery.dataTables.min.js"></script>
+<script src="{{ asset('myjs/jquery.dataTables.min.js') }}"></script>
 
-<script src="/myjs/jquery.slimscroll.js"></script>
+<script src="{{ asset('myjs/jquery.slimscroll.js') }}"></script>
 
-<script src="/myjs/toast-data.js"></script>
+<script src="{{ asset('myjs/toast-data.js') }}"></script>
 
-<script src="/myjs/moment.min.js"></script>
-<script src="/myjs/jquery.simpleWeather.min.js"></script>
-<script src="/myjs/simpleweather-data.js"></script>
+<script src="{{ asset('myjs/moment.min.js') }}"></script>
+<script src="{{ asset('myjs/jquery.simpleWeather.min.js') }}"></script>
+<script src="{{ asset('myjs/simpleweather-data.js') }}"></script>
 
-<script src="/myjs/jquery.waypoints.min.js"></script>
-<script src="/myjs/jquery.counterup.min.js"></script>
+<script src="{{ asset('myjs/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('myjs/jquery.counterup.min.js') }}"></script>
 
-<script src="/myjs/dropdown-bootstrap-extended.js"></script>
+<script src="{{ asset('myjs/dropdown-bootstrap-extended.js') }}"></script>
 
-<script src="/myjs/jquery.sparkline.min.js"></script>
+<script src="{{ asset('myjs/jquery.sparkline.min.js') }}"></script>
 
-<script src="/myjs/jquery.easypiechart.min.js"></script>
+<script src="{{ asset('myjs/jquery.easypiechart.min.js') }}"></script>
 
 
 
-<script src="/myjs/owl.carousel.min.js"></script>
+<script src="{{ asset('myjs/owl.carousel.min.js') }}"></script>
 
-<script src="/myjs/Chart.min.js"></script>
+<script src="{{ asset('myjs/Chart.min.js') }}"></script>
 
-<script src="/myjs/raphael.min.js"></script>
-<script src="/myjs/morris.min.js"></script>
-<script src="/myjs/jquery.toast.min.js"></script>
+<script src="{{ asset('myjs/raphael.min.js') }}"></script>
+<script src="{{ asset('myjs/morris.min.js') }}"></script>
+<script src="{{ asset('myjs/jquery.toast.min.js') }}"></script>
 
-<script src="/myjs/switchery.min.js"></script>
+<script src="{{ asset('myjs/switchery.min.js') }}"></script>
 
-<script src="/myjs/init.js"></script>
-<script src="/myjs/dashboard-data.js"></script>
+<script src="{{ asset('myjs/init.js') }}"></script>
+<script src="{{ asset('myjs/dashboard-data.js') }}"></script>
