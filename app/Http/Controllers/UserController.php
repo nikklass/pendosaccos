6eee64b671500b5745fc8cd7826c6657e4079837
Nicholas Kute <heavybitent@gmail.com>
2017-08-01 22:38:41 +0300

    change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,7 +70,7 @@ class UserController extends Controller
 
         }
 
-        dd($users, $companies);
+        //dd($users, $companies);
 
         return view('users.index')
                 ->withUser($user)
